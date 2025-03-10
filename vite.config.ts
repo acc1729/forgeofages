e5d026577e55a93c96dec663939769d554c81e4c
Ben Straub <ben@straub.cc>
2025-03-10 06:54:10 -0700

    GHA to build and deploy to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/forgesteel/',
+	base: '/forgeofages/',
 	build: {
 		chunkSizeWarningLimit: 5000
 	},
