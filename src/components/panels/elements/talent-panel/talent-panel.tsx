4bab693d1c831a76e4d7777f8e8ec36697f387fd
Ben Straub <ben@straub.cc>
2025-03-13 09:05:29 -0700

    Render talent feats

--- src/components/panels/elements/talent-panel/talent-panel.tsx
+++ src/components/panels/elements/talent-panel/talent-panel.tsx
@@ -5,6 +5,8 @@ import { Markdown } from '../../../controls/markdown/markdown';
 import { Options } from '../../../../models/options';
 import { PanelMode } from '../../../../enums/panel-mode';
 import { Talent } from '../../../../models/talent';
+import { TierAbbreviations } from '../../../../enums/tier';
+import { Feat } from '../../../../models/feat';
 
 import './talent-panel.scss';
 
@@ -16,6 +18,23 @@ interface Props {
 	options?: Options;
 	mode?: PanelMode;
 	tags?: string[];
+}
+
+function renderFeats(feats: Feat[]) {
+	if (feats.length === 0) {
+		return null;
+	}
+	return (
+		<div>
+			<HeaderText level={3}>Feats</HeaderText>
+			{feats.map((feat, i) => (
+					<p key={i}>
+						<Markdown text={`**${TierAbbreviations[feat.tier]}:** ${feat.description}`} />
+					</p>
+				))}
+		</div>
+	);
+
 }
 
 export const TalentPanel = (props: Props) => {
@@ -31,6 +50,7 @@ export const TalentPanel = (props: Props) => {
 					{props.talent.name || 'Unnamed Talent'}
 				</HeaderText>
 				<Markdown text={props.talent.description} />
+				{props.talent.feats.length > 0 ? renderFeats(props.talent.feats) : null}
 			</div>
 		);
 	} catch (ex) {
--- src/enums/tier.ts
+++ src/enums/tier.ts
@@ -1,5 +1,20 @@
 export enum Tier {
 	Adventurer,
 	Champion,
-	Epic
+	Epic,
+	Zenith,
+};
+
+export const TierLabels = {
+	[Tier.Adventurer]: 'Adventurer',
+	[Tier.Champion]: 'Champion',
+	[Tier.Epic]: 'Epic',
+	[Tier.Zenith]: 'Zenith'
+};
+
+export const TierAbbreviations = {
+	[Tier.Adventurer]: 'A',
+	[Tier.Champion]: 'C',
+	[Tier.Epic]: 'E',
+	[Tier.Zenith]: 'Z'
 };
