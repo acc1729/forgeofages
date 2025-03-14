362b56c870f6cef06211d39f709b673b94127052
Ben Straub <ben@straub.cc>
2025-03-13 20:22:08 -0700

    Barbarian talent feats

--- src/data/classes/13a-barbarian.ts
+++ src/data/classes/13a-barbarian.ts
@@ -57,31 +57,130 @@ export const barbarian: HeroClass = {
 			name: 'Barbaric Cleave',
 			description: 'Once per battle as a free action, make another barbarian melee attack after you have dropped a non-mook foe to 0 hp with a barbarian melee attack. (Dropping the last mook of a mook mob also qualifies you to use Barbaric Cleave.)',
 			tier: Tier.Adventurer,
-			cost: 2,
+			feats: [
+				FactoryLogic.createFeat({
+					id: 'barbaric-cleave-feat-a',
+					name: 'Feat: Barbaric Cleave A',
+					tier: Tier.Adventurer,
+					description: `You gain a +2 attack bonus with Barbaric Cleave attacks. If the cleave attack hits, you can heal using a recovery.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'barbaric-cleave-feat-c',
+					name: 'Feat: Barbaric Cleave C',
+					tier: Tier.Champion,
+					description: `If there is no foe engaged with you to use your Barbaric Cleave attack against, as a free action you can move to a nearby foe before making the attack.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'barbaric-cleave-feat-e',
+					name: 'Feat: Barbaric Cleave E',
+					tier: Tier.Epic,
+					description: `While raging, you can use Barbaric Cleave as many times as you like during a battle, but only once per round.`,
+				}),
+			],
 		}),
 		FactoryLogic.createTalent({
 			id: 'building-frenzy',
 			name: 'Building Frenzy',
 			description: 'One battle per day, as a free action after you have missed with an attack, deal +1d4 damage with your melee attacks until the end of the battle. Deal +1d4 additional damage each time one of your attacks misses, up to a maximum of +4d4 damage.',
 			tier: Tier.Adventurer,
+			feats: [
+				FactoryLogic.createFeat({
+					id: 'building-frenzy-feat-a',
+					name: 'Feat: Building Frenzy A',
+					tier: Tier.Adventurer,
+					description: `Bonus damage dice are now d6s.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'building-frenzy-feat-c',
+					name: 'Feat: Building Frenzy C',
+					tier: Tier.Champion,
+					description: `Bonus damage dice are now d10s.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'building-frenzy-feat-e',
+					name: 'Feat: Building Frenzy E',
+					tier: Tier.Epic,
+					description: `You can use Building Frenzy twice a day.`,
+				}),
+			],
 		}),
 		FactoryLogic.createTalent({
 			id: 'slayer',
 			name: 'Slayer',
 			description: 'During your turn, when you attack a staggered enemy you were not engaged with at the start of your turn, deal +1d6 damage per level to that creature if you hit.',
 			tier: Tier.Adventurer,
+			feats: [
+				FactoryLogic.createFeat({
+					id: 'slayer-feat-a',
+					name: 'Feat: Slayer A',
+					tier: Tier.Adventurer,
+					description: `You gain a +2 bonus to Slayer attacks.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'slayer-feat-c',
+					name: 'Feat: Slayer C',
+					tier: Tier.Champion,
+					description: `Once per battle, when you miss with a Slayer attack, deal the additional +1d6-per-level damage to the target instead of normal miss damage.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'slayer-feat-e',
+					name: 'Feat: Slayer E',
+					tier: Tier.Epic,
+					description: `Whenever one of your Slayer attacks drops a non-mook enemy to 0 hp, you gain 20 temporary hit points.`,
+				}),
+			],
 		}),
 		FactoryLogic.createTalent({
 			id: 'strongheart',
 			name: 'Strongheart',
 			description: 'Your recovery dice are d12s instead of d10s like other barbarians.',
 			tier: Tier.Adventurer,
+			feats: [
+				FactoryLogic.createFeat({
+					id: 'strongheart-feat-a',
+					name: 'Feat: Strongheart A',
+					tier: Tier.Adventurer,
+					description: `Increase your total number of recoveries by 1.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'strongheart-feat-c',
+					name: 'Feat: Strongheart C',
+					tier: Tier.Champion,
+					description: `You gain +1 PD. When you heal using a recovery, you can roll a save against a save ends effect.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'strongheart-feat-e',
+					name: 'Feat: Strongheart E',
+					tier: Tier.Epic,
+					description: `Increase your total number of recoveries by 1 (making a total of +2 from this talent).`,
+				}),
+			],
 		}),
 		FactoryLogic.createTalent({
 			id: 'unstoppable',
 			name: 'Unstoppable',
 			description: "Once per battle, declare you're using Unstoppable before making a barbarian melee attack. If your attack hits at least one target, you can heal using a recovery.",
 			tier: Tier.Adventurer,
+			feats: [
+				FactoryLogic.createFeat({
+					id: 'unstoppable-feat-a',
+					name: 'Feat: Unstoppable A',
+					tier: Tier.Adventurer,
+					description: `The Unstoppable recovery is free.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'unstoppable-feat-c',
+					name: 'Feat: Unstoppable C',
+					tier: Tier.Champion,
+					description: `Add double your Constitution modifier to the healing the recovery provides.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'unstoppable-feat-e',
+					name: 'Feat: Unstoppable E',
+					tier: Tier.Epic,
+					description: `You can use Unstoppable twice per battle.`,
+				}),
+			],
 		}),
 		FactoryLogic.createTalent({
 			id: 'whirlwind',
@@ -89,18 +188,72 @@ export const barbarian: HeroClass = {
 			description: `You can make a Whirlwind attack as the first action of your turn when you are engaged by two or more enemies.
 			You take a -4 penalty to your AC and PD until the start of your next turn. Then roll a separate melee attack against each enemy you are engaged with. You deal no miss damage with these attacks.`,
 			tier: Tier.Adventurer,
+			feats: [
+				FactoryLogic.createFeat({
+					id: 'whirlwind-feat-a',
+					name: 'Feat: Whirlwind A',
+					tier: Tier.Adventurer,
+					description: `You now deal normal miss damage with missed Whirlwind attacks.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'whirlwind-feat-c',
+					name: 'Feat: Whirlwind C',
+					tier: Tier.Champion,
+					description: `The penalty to your AC and PD is reduced to –2. In addition, disengage checks you make the same turn as using Whirlwind automatically succeed.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'whirlwind-feat-e',
+					name: 'Feat: Whirlwind E',
+					tier: Tier.Epic,
+					description: `You can use Whirlwind anytime during your turn, not just as the first action.`,
+				}),
+			],
 		}),
 		FactoryLogic.createTalent({
 			id: 'natural-will',
 			name: 'Natural Will',
 			description: 'One battle per day as a quick action, you gain a +2 bonus to your Mental Defense until the end of the battle.',
 			tier: Tier.Champion,
+			feats: [
+				FactoryLogic.createFeat({
+					id: 'natural-will-feat-a',
+					name: 'Feat: Natural Will A',
+					tier: Tier.Adventurer,
+					description: `You can now use Natural Will in two battles per day.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'natural-will-feat-c',
+					name: 'Feat: Natural Will C',
+					tier: Tier.Champion,
+					description: `The bonus increases to +4 Mental Defense.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'natural-will-feat-e',
+					name: 'Feat: Natural Will E',
+					tier: Tier.Epic,
+					description: `You can now use Natural Will as a free action when an enemy attacks you.`,
+				}),
+			],
 		}),
 		FactoryLogic.createTalent({
 			id: 'violence',
 			name: 'Violence',
 			description: 'Once per battle, add a +1d4 bonus to a barbarian melee attack roll after finding out whether you hit or missed.',
 			tier: Tier.Champion,
+			feats: [
+				FactoryLogic.createFeat({
+					id: 'violence-feat-c',
+					name: 'Feat: Violence C',
+					tier: Tier.Champion,
+					description: `If the attack still misses, deal half damage.`,
+				}),
+				FactoryLogic.createFeat({
+					id: 'violence-feat-e',
+					name: 'Feat: Violence E',
+					tier: Tier.Epic,
+					description: `The bonus increases to +1d6.`,
+				}),
+			],
 		}),
 		FactoryLogic.createTalent({
 			id: 'ancestral-warband',
@@ -108,12 +261,28 @@ export const barbarian: HeroClass = {
 			description: `One battle per day as a quick action, you can call the spirits of your ancestors to fight alongside you. Explain the visuals any way you like; your ancestors roar in from the spirit world and can't be hurt or affected by the creatures of this world.
 			At the end of each of your turns, roll a d6 if you are conscious. If you roll less than or equal to the escalation die, a member of your spirit warband strikes from the spirit world into the world. Make a melee attack against a nearby enemy as if you were making the attack yourself, using any talents/feats/magic items/etc. as you see fit. The attack doesn't take any of your actions.`,
 			tier: Tier.Epic,
+			feats: [
+				FactoryLogic.createFeat({
+					id: 'ancestral-warband-feat-e',
+					name: 'Feat: Ancestral Warband E',
+					tier: Tier.Epic,
+					description: `Your Ancestral Warband spirits are always raging, even if you are not, and continue to fight for a single round while you are unconscious.`,
+				}),
+			],
 		}),
 		FactoryLogic.createTalent({
 			id: 'relentless',
 			name: 'Relentless',
 			description: 'While raging, you have resist damage 12+ (when an attack targets you, the attacker must roll a natural 12 or higher on the attack roll or it only deals half damage).',
 			tier: Tier.Epic,
+			feats: [
+				FactoryLogic.createFeat({
+					id: 'relentless-feat-e',
+					name: 'Feat: Relentless E',
+					tier: Tier.Epic,
+					description: `Even when not raging, whenever you score a critical hit against an enemy, you have resist damage 12+ until the start of your next turn.`,
+				}),
+			],
 		}),
 	],
 	abilities: [],
